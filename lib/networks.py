e4b39af894151f27dca16da2cba4a7e0f8d898a8
Z. Tang <chuang.tang@yale.edu>
2019-08-01 12:37:39 -0500

    Convert to full working Sonnet modules

--- lib/networks.py
+++ lib/networks.py
@@ -1,76 +1,90 @@
 import sonnet as snt
 import tensorflow as tf
 from tensorflow.contrib.layers import instance_norm
-from tensorflow.keras.layers import Conv2D, ReLU, UpSampling2D, MaxPool2D, Dense, Flatten
+from tensorflow.keras.layers import ReLU, UpSampling2D, MaxPool2D
 
 
-def down_block(x, filters, downsample=True, **kwargs):
-  x = Conv2D(filters, 3, activation='relu', padding='same', kernel_initializer='truncated_normal', use_bias=False)(x)
-  x = instance_norm(x)
-  x = residual = ReLU()(x)
-  if downsample:
-    x = MaxPool2D()(residual)
-  return x, residual
+class DownBlock(snt.AbstractModule):
+  def __init__(self, name='down_block', **kwargs):
+    super(DownBlock, self).__init__(name=name)
+    self._downsample = kwargs['downsample']
+    self._filters = kwargs['filters']
+
+  def _build(self, x):
+    x = snt.Conv2D(self._filters, 3, padding='SAME', use_bias=False)(x)
+    x = instance_norm(x)
+    x = residual = ReLU()(x)
+    if self._downsample:
+      x = MaxPool2D()(residual)
+    return x, residual
 
 
-def up_block(x, residual, filters, upsample=True, **kwargs):
-  x = tf.concat([x, residual], axis=3)
-  x = Conv2D(filters, 3, activation='relu', padding='same', kernel_initializer='truncated_normal', use_bias=False)(x)
-  x = instance_norm(x)
-  x = ReLU()(x)
-  if upsample:
-    x = UpSampling2D(interpolation='nearest')(x)
-  return x
+class UpBlock(snt.AbstractModule):
+  def __init__(self, name='up_block', **kwargs):
+    super(UpBlock, self).__init__(name=name)
+    self._upsample = kwargs['upsample']
+    self._filters = kwargs['filters']
+
+  def _build(self, x, residual):
+    x = tf.concat([x, residual], axis=3)
+    x = snt.Conv2D(self._filters, 3, padding='SAME', use_bias=False)(x) # default initializer is a truncated normal! how handy
+    x = instance_norm(x)
+    x = ReLU()(x)
+    if self._upsample:
+      x = UpSampling2D(interpolation='bilinear')(x)
+    return x
 
 
-def mlp(y, **kwargs):
-  """This MLP returns a tensor in the original shape as the input"""
-  h, w, c = y.shape[1:]
-  y = Flatten()(y)
-  for t in kwargs['mlp_sizes']:
-    y = Dense(t, activation='relu')(y)
-  y = Dense(h * w * c, activation='relu')(y)
-  y = tf.reshape(y, (-1, h, w, c))
-  return y
+class UNet(snt.AbstractModule):
+  def __init__(self, name='u_net', **kwargs):
+    super(UNet, self).__init__(name=name)
+    self._numBlocks = kwargs['num_blocks']
+    self._filters = kwargs['filters']
+    self._mlp = kwargs['mlp_sizes']
+
+  def _build(self, x):
+    residuals = []
+    for i in range(self._numBlocks):
+      to_downsample = i < (self._numBlocks - 1)
+      x, residual = DownBlock(filters=self._filters[i], downsample=to_downsample)(x)
+      residuals.append(residual)
+
+    # the non-residual block is a 3-layer MLP
+    h, w, c = x.get_shape().as_list()[1:]
+    y = snt.BatchFlatten()(x)
+    y = snt.nets.MLP(
+      output_sizes=self._mlp + [h*w*c],
+      name='mlp'
+    )(y)
+    y = snt.BatchReshape(shape=(h, w, c))(y)
+
+    for i in range(self._numBlocks):
+      to_upsample = i < (self._numBlocks - 1)
+      y = UpBlock(filters=self._filters[-(i + 1)],
+                   upsample=to_upsample)(y, residuals[-(i + 1)])
+
+    # Finally, you get a 1x1 channel
+    y = snt.Conv2D(1, 1)(y)
+    return y
 
 
-def unet(x, **kwargs):
-  residuals = []
-  for i in range(kwargs['num_blocks']):
-    to_downsample = i < (kwargs['num_blocks'] - 1)
-    x, residual = down_block(x, filters=kwargs['filters'][i], downsample=to_downsample)
-    residuals.append(residual)
+class AttentionNetwork(snt.AbstractModule):
+  def __init__(self, name='attention_network', **kwargs):
+    super(AttentionNetwork, self).__init__(name=name)
+    self._kwargs = kwargs
 
-  # the non-residual block is a 3-layer MLP
-  y = mlp(x, **kwargs)
+  def _build(self, image, log_attention_scope):
+    x = tf.concat([image, log_attention_scope], axis=3)
+    log_min = 1e-10
+    log_max = 1e8
 
-  for i in range(kwargs['num_blocks']):
-    to_upsample = i < (kwargs['num_blocks'] - 1)
-    y = up_block(y, residuals[-(i + 1)],
-                 filters=kwargs['filters'][-(i + 1)],
-                 upsample=to_upsample)
-
-  # Finally, you get a 1x1 channel
-  y = Conv2D(1, 1, activation='relu', kernel_initializer='truncated_normal')(y)
-  return y
-
-
-def attention_network(image, log_attention_scope, iteration, **kwargs):
-  x = tf.concat([image, log_attention_scope], axis=3)
-  log_min = 1e-10
-  log_max = 1e8
-
-  if iteration == kwargs['attention_steps'] - 1:
-    log_mask = log_attention_scope
-    log_next_scope = None
-  else:
-    logits = unet(log_attention_scope, **kwargs)
+    logits = UNet(**self._kwargs)(log_attention_scope)
     log_alpha_k = tf.math.log_sigmoid(logits)
     log_mask = log_attention_scope + log_alpha_k
     log_next_scope = log_attention_scope + tf.math.log(
       tf.clip_by_value((1 - tf.exp(log_alpha_k)), clip_value_min=log_min, clip_value_max=log_max))
 
-  return log_mask, log_next_scope
+    return log_mask, log_next_scope
 
 
 def expand(tensor, *dims):
@@ -103,6 +117,12 @@ def broadcast_decoder(z, **kwargs):
   return tf.concat([x, y, zs], axis=-1)
 
 
+def sample(latents):
+  z_mu, z_log_sigma = tf.split(latents, num_or_size_splits=2, axis=1)
+  z_samples = tf.random.normal(shape=z_mu.shape) * tf.exp(z_log_sigma * .5) + z_mu
+  return z_samples
+
+
 class ComponentVAE(snt.AbstractModule):
 
   def __init__(self, name='component_vae', **kwargs):
@@ -112,55 +132,44 @@ class ComponentVAE(snt.AbstractModule):
     self.decoder_args = self.kwargs['cvae_decoder']
     self.latent_dim = self.encoder_args['mlp'][-1] // 2
 
-    self.encoder_cnn = snt.nets.ConvNet2D(
+  @snt.reuse_variables
+  def encode(self, inputs):
+    outputs = snt.nets.ConvNet2D(
       output_channels=self.encoder_args['output_channels'],
       kernel_shapes=self.encoder_args['kernel_shapes'],
       strides=self.encoder_args['strides'],
       paddings=[snt.SAME],
       # By default final layer activation is disabled.
       activate_final=True,
-      name='convolutional_module'
-    )
-    self.encoder_mlp = snt.nets.MLP(
+      name='encoder'
+    )(inputs)
+    outputs = snt.BatchFlatten()(outputs)
+    outputs = snt.nets.MLP(
       output_sizes=self.encoder_args['mlp'],
       name='fully_connected_module'
-    )
-    self.decoder_cnn = snt.nets.ConvNet2D(
+    )(outputs)
+    return outputs
+
+
+  @snt.reuse_variables
+  def decode(self, latents):
+    decoder_inputs = broadcast_decoder(latents, **self.kwargs)
+    outputs = snt.nets.ConvNet2D(
       output_channels=self.decoder_args['output_channels'],
       kernel_shapes=self.decoder_args['kernel_shapes'],
       strides=self.decoder_args['strides'],
       paddings=[snt.VALID],
       # By default final layer activation is disabled.
       activate_final=True,
-      name='convolutional_module_too'
-    )
-
-
-  def encode(self, inputs):
-    outputs = self.encoder_cnn(inputs)
-    outputs = snt.BatchFlatten()(outputs)
-    outputs = self.encoder_mlp(outputs)
+      name='decoder'
+    )(decoder_inputs)
     return outputs
-
-
-  def sample(self, latents):
-    bs = latents.shape[0]
-    z_mu, z_log_sigma = tf.split(latents, num_or_size_splits=2, axis=1)
-    z_samples = tf.random.normal(shape=(bs, self.latent_dim)) * tf.exp(z_log_sigma * .5) + z_mu
-    return z_samples
-
-
-  def decode(self, latents):
-    decoder_inputs = broadcast_decoder(latents, **self.kwargs)
-    outputs = self.decoder_cnn(decoder_inputs)
-    return outputs
-
 
   def _build(self, inputs):
     """ Build the model stack.
     """
     latents = self.encode(inputs)
-    z = self.sample(latents)
+    z = sample(latents)
     outputs = self.decode(z)
     rgb_image, reconstructed_mask = outputs[:,:,:,:3], outputs[:,:,:,3]
     return rgb_image, reconstructed_mask
@@ -170,35 +179,42 @@ class MONet(snt.AbstractModule):
 
   def __init__(self, name='monet', **kwargs):
     super().__init__(name=name)
-
-    self.kwargs = kwargs
-    self.attention_steps = self.kwargs['attention_steps']
-    self.cvae = ComponentVAE(**kwargs)
+    self._kwargs = kwargs
+    self._attention_steps = self._kwargs['attention_steps']
+    with self._enter_variable_scope():
+      self.attention = AttentionNetwork(**self._kwargs)
+      self.cvae = ComponentVAE(**self._kwargs)
 
   def _build(self, image, **kwargs):
+    print('OK, building the thing')
     batch_size, h, w, c = image.shape
-    scope = tf.zeros((batch_size, h, w, 1))
+    log_attention_scope = tf.zeros((batch_size, h, w, 1))
 
     endpoints = {}
     endpoints['attention_mask'] = []
     endpoints['reconstruct_mask'] = []
     endpoints['reconstruct_image'] = []
 
-    reconstructed_image = tf.zeros_like(image)
-    for i in range(self.attention_steps):
-      # feed it to our attention network
-      log_mask, scope = attention_network(image, scope, iteration=i, **self.kwargs)
-      attention_mask = tf.exp(log_mask)
+    reconstructed_image = tf.zeros_like(image, name='image_reconstructed')
+    for i in range(self._attention_steps):
+      if i < self._attention_steps - 1:
+        print('Attention step', i)
+        log_mask, log_attention_scope = self.attention(image, log_attention_scope)
+      else:
+        log_mask = log_attention_scope
+      attention_mask = tf.exp(log_mask, name='mask_attention')
       endpoints['attention_mask'].append(attention_mask)
 
       # feed it to the VAE
       object_mean, mask_logits = self.cvae(tf.concat([image, log_mask], axis=3))
       noise_scale = hparams['component_scale_background'] if i == 0 else hparams['component_scale_foreground']
-      object_image = tf.random.normal(object_mean.shape) * noise_scale + object_mean
-      reconstructed_mask = tf.sigmoid(mask_logits)
+      with tf.name_scope('obj_image'):
+        object_image = tf.random.normal(object_mean.shape) * noise_scale + object_mean
+        reconstructed_image += object_image
+      reconstructed_mask = tf.sigmoid(mask_logits, name='mask_reconstructed')
       endpoints['reconstruct_mask'].append(reconstructed_mask)
       endpoints['reconstruct_image'].append(object_image)
-      reconstructed_image += object_image
+
 
     return reconstructed_image, endpoints
 
@@ -224,7 +240,7 @@ def test_separate(image, scope, hparams):
   # feed it to our attention network
   masks = []
   for i in range(hparams['attention_steps']):
-    log_mask, scope = attention_network(image, scope, iteration=i, **hparams)
+    log_mask, scope = AttentionNetwork(hparams)(image, scope, iteration=i)
     masks.append(tf.exp(log_mask))
 
     # feed it to the VAE
@@ -257,16 +273,22 @@ def test_separate(image, scope, hparams):
   check_attention_masks(masks)
 
 
-def test_monet(image, scope, hparams):
+def test_monet(image, scope, eager=False, **hparams):
   m = MONet(**hparams)
   reconstructed_image, endpoints = m(image)
 
-  print('Reconstructed image', image.shape)
-  plt.imshow(tf.squeeze(reconstructed_image[0]))
-  plt.title('Reconstructed image')
-  plt.show()
+  if eager:
+    print('Reconstructed image', image.shape)
+    plt.imshow(tf.squeeze(reconstructed_image[0]))
+    plt.title('Reconstructed image')
+    plt.show()
 
-  check_attention_masks(endpoints['attention_mask'])
+    check_attention_masks(endpoints['attention_mask'])
+  else:
+    with tf.Session() as sess:
+      sess.run(tf.global_variables_initializer())
+      writer = tf.summary.FileWriter("temp_graph", sess.graph)
+      writer.close()
 
 
 if __name__ == '__main__':
@@ -277,7 +299,7 @@ if __name__ == '__main__':
   import matplotlib.pyplot as plt
   from configs.mini import hparams
 
-  tf.enable_eager_execution()
+  # tf.enable_eager_execution()
 
   # make an image + scope
   batch_size = 2
@@ -287,4 +309,4 @@ if __name__ == '__main__':
   sd[0, 0:10, 0:10, 0] = 0
   scope = tf.convert_to_tensor(sd, dtype=tf.float32)
 
-  test_monet(image, scope, hparams)
+  test_monet(image, scope, eager=False, **hparams)
