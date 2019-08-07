b1b4eb3cc8284e0bec135c7ccb35fb9613893e1f
Z. Tang <chuang.tang@yale.edu>
2019-08-07 14:50:12 -0500

    Add configurable output dir when generating data

--- configs/datagen.py
+++ configs/datagen.py
@@ -3,7 +3,8 @@
 # Define Config
 hparams = {}
 hparams['n_scenes'] = 2e3
+hparams['output_dir'] = 'data12'
 hparams['data_splits'] = {
   # 3-6 objects in our scenes
-  'n_objects': [0.0, 0.0, 0.0, 0.0, 1.0, 0.0]
+  'n_objects': [1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 }
