521504da482795c27113256fadf931ff0b0802f8
Z. Tang <chuang.tang@yale.edu>
2019-08-07 14:45:51 -0500

    Change VAE model
    Directly compute the loss
    Restructures the Model base class to only have build_model as a method

--- lib/model.py
+++ lib/model.py
@@ -40,90 +40,100 @@ def set_flags(flags):
 
 
 class Model(object):
-  """Base model"""
+  """Base class
+
+  The point of the model is to wrap a network with some data,
+  a loss function, an op to train it, and possible methods
+  for working with the network. For example, in a generative model
+  you might have some `generate()` methods.
+
+  To define a new model, just implement `define_train_op` to return a
+  new train op. Happy subclassing!
+  """
 
   def __init__(self, batch_size, config):
-    """ Builds a model that you can train!
-
-    Args:
-      batch_size: (int) Build graph with fixed batch size.
-      config: (dict) All the global state.
+    """Sets up a model with a `train_op`
     """
     data_helper = data_helpers.registry[config['data_type']](config)
     real_images = data_helper.provide_data(batch_size)
 
     self.config = config
+    self.batch_size = batch_size
     self.data_helper = data_helper
     self.real_images = data_helper.provide_data(batch_size)
-
     self.build_model(config)
-    self.define_train_op(config)
 
   def build_model(self, config):
-    raise NotImplementedError
-
-  def define_train_op(self, config):
-    """Returns a train_op"""
+    """
+    Usually:
+    1) Define the model that takes `real_images` as input
+    2) Define loss
+    3) Define optimizer + train_op
+    """
     raise NotImplementedError
 
   def add_summaries(self):
-    raise NotImplementedError
+    """Logs the input data by default"""
+    tf.summary.histogram('input', self.real_images[0])
+    tf.summary.image('input images', self.real_images)
+
 
 class VAEModel(Model):
   """The component VAE by itself"""
+
   def build_model(self, config):
-    vae = networks.ComponentVAE(**config)
+    # our VAE takes as input: log attention masks
+    _, h, w, c = self.real_images.get_shape().as_list()
+    log_attention = tf.zeros((self.batch_size, h, w, 1))
 
-    batch_size, h, w, c = self.real_images.shape
-    full_mask = tf.zeros((batch_size, h, w, 1))
-    object_logits, mask_logits, latents = vae(tf.concat([self.real_images, full_mask], axis=3))
-
-    self.attn_mask = [full_mask]
-    self.obj_mask = [tf.nn.sigmoid(mask_logits)]
-    self.obj_image = [tf.nn.sigmoid(object_logits)]
-    self.latents = [latents]
-
-  def define_train_op(self, config):
-    with tf.name_scope('reconstruction_loss'):
-      reconstruction_loss = train_util.reconstruction_loss(
-        self.obj_image, self.attn_mask, self.real_images,
-        background_scale=config['component_scale_background'],
-        foreground_scale=config['component_scale_foreground'])
-
-    # compute the VAE latent loss
-    with tf.name_scope('vae_loss'):
-      cvae_loss = 0
-      for latents in self.latents:
-        z_mu, z_log_var = tf.split(latents, num_or_size_splits=2, axis=1)
-        cvae_loss += train_util.vae_latent_loss(z_mu, z_log_var)
-
-    self.reconstruction_loss = reconstruction_loss
-    self.cvae_loss = cvae_loss
-    self.total_loss = reconstruction_loss + cvae_loss
-
-    train_op, optimizer = train_util.define_train_ops(
-      self.total_loss, **config
+    # and produces unnormalized versions of the inputs
+    object_logits, mask_logits, latents = networks.ComponentVAE(**config)(
+      tf.concat([self.real_images, log_attention], axis=3)
     )
-    self.train_op = train_op
+
+    # In order to make the mask_logits form valid masks, we will
+    # do a softmax over all the K attention steps. In this case, there
+    # is only 1 step.
+    mask = tf.sigmoid(mask_logits)
+
+    # Let's look at the reconstructed images! This is not necessary to compute
+    # the reconstruction loss (as we'll see later)
+    obj_image = tf.sigmoid(object_logits)
+    masked_image = tf.multiply(mask, obj_image)
+
+    ##### So how DO we compute the reconstruction loss?
+    # Gaussian decoder distribution p_(x| z_k)
+    variance = config['component_scale_background']
+    log_likelihood = (-0.5 * tf.log(variance) - tf.square(obj_image - self.real_images) / (2 * variance))
+    rec_loss = -tf.reduce_logsumexp(log_attention + log_likelihood, axis=[1,2,3])
+    reconstruction_loss = tf.reduce_sum(rec_loss)
+
+    # Part 2: KL divergence between the latents and our prior N(0,1)
+    mu, logvar = tf.split(latents, num_or_size_splits=2, axis=1)
+    KLD = -0.5 * tf.reduce_sum(1 + logvar - tf.square(mu) - tf.exp(logvar))
+
+    self.endpoints = {
+      'image': object_logits,
+      'mask': mask_logits,
+      'latents': latents,
+      'obj_image': obj_image,
+      'masked_image': masked_image,
+      'reconstruction_loss': reconstruction_loss,
+      'KL_loss': KLD
+    }
+
+    self.total_loss = reconstruction_loss + KLD
+    self.train_op, optimizer = train_util.define_train_ops(self.total_loss, **config)
 
   def add_summaries(self):
-    """Adds model summaries."""
+    super(VAEModel, self).add_summaries()
+    scalars = ['reconstruction_loss', 'KL_loss']
+    images = ['obj_image', 'masked_image']
 
-    def _log_many(name, images):
-      for i, m in enumerate(images):
-        tf.summary.image(f'step{i}/{name}', m)
-        tf.summary.histogram(f'step{i}/{name}', m[0])
-
-    _log_many('cvae_mask', self.obj_mask)
-    _log_many('cvae_image', self.obj_image)
-
-    tf.summary.image('input_images', self.real_images)
-
-    tf.summary.histogram('input', self.real_images[0])
-
-    tf.summary.scalar('normal_vae_KL', self.cvae_loss)
-    tf.summary.scalar('reconstruction_loss', self.reconstruction_loss)
-    tf.summary.scalar('total_loss', self.total_loss)
+    for i in scalars:
+      tf.summary.scalar(i, self.endpoints[i])
+    for i in images:
+      tf.summary.image(i, self.endpoints[i])
 
 
 class MonetModel(Model):
@@ -180,17 +190,14 @@ class MonetModel(Model):
     self.reconstruction_loss = reconstruction_loss
     self.total_loss = loss
 
-
-
-  def define_train_op(self, config):
-    train_op, optimizer = train_util.define_train_ops(
+    self.train_op, optimizer = train_util.define_train_ops(
       self.total_loss, **config
     )
-    self.train_op = train_op
 
 
   def add_summaries(self):
     """Adds model summaries."""
+    super(MonetModel, self).add_summaries()
 
     def _log_many(name, images):
       for i, m in enumerate(images):
@@ -201,7 +208,6 @@ class MonetModel(Model):
     _log_many('cvae_mask', self.obj_masks)
     _log_many('cvae_image', self.obj_images)
 
-    tf.summary.image('input_images', self.real_images)
     tf.summary.image('inferred_images', self.reconstructed_images)
 
     tf.summary.scalar('attention_KL', self.attention_loss)
@@ -216,10 +222,11 @@ class AttentionModel(Model):
   def build_model(self, config):
     self.attention = networks.AttentionNetwork(**config)
 
-    # provide a fake datasource for masks
-    batch_size, h, w, c = self.real_images.shape
+    ##### provide a fake datasource for masks
+    batch_size, h, w, c = self.real_images.get_shape().as_list()
     next_scope = tf.zeros((batch_size, h, w, 1))
 
+    ##### Forward pass
     scopes = [next_scope]
     masks = []
     for i in range(config['attention_steps'] - 1):
@@ -228,23 +235,18 @@ class AttentionModel(Model):
       masks.append(mask)
     masks.append(next_scope)
 
+    masks_tensor = tf.exp(tf.concat(masks[1:], axis=c))
+    reconstructed_image = tf.minimum(c * masks_tensor, 1.0)
+
+    ##### Add variables as properties
     self.masks = masks
     self.scopes = scopes
-
-
-  def define_train_op(self, config):
-    """
-    Should be much easier to train! (they're essentially independent)
-
-    For this second part, we're going to make the LAST THREE masks count
-    Let's see if this trains better
-    """
-    c = self.real_images.get_shape().as_list()[-1]
-    masks = tf.exp(tf.concat(self.masks[1:], axis=c))
-    reconstructed_image = tf.minimum(c * masks, 1.0)
-    self.total_loss = tf.losses.mean_squared_error(self.real_images, reconstructed_image)
-    self.train_op, _ = train_util.define_train_ops(self.total_loss, **config)
     self.reconstructed_image = reconstructed_image
+    self.total_loss = tf.losses.mean_squared_error(self.real_images, reconstructed_image)
+
+    self.train_op, optimizer = train_util.define_train_ops(
+      self.total_loss, **config
+    )
 
 
   def add_summaries(self):
@@ -255,11 +257,7 @@ class AttentionModel(Model):
 
     _log_many('mask', [tf.exp(x) for x in self.masks])
     _log_many('scope', [tf.exp(x) for x in self.scopes])
-    
-    tf.summary.histogram('input', self.real_images[0])
-
     tf.summary.image('inferred_image', self.reconstructed_image)
-    tf.summary.image('input images', self.real_images)
     tf.summary.scalar('loss', self.total_loss)
 
 registry = {
